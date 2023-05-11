8ae7d4b4a85a0f63dc61b5549277b05434f8daf6
nivznivz0@gmail.com <nivznivz0@gmail.com>
2023-05-11 19:21:46 +0300

    add queue structure

---
+++ src/structures/queue.ts
@@ -0,0 +1,19 @@
+export class Queue<T> {
+  private queue: T[];
+
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue = (item: T) => {
+    this.queue.push(item);
+  };
+
+  dequeue = () => {
+    return this.queue.shift();
+  };
+
+  size = () => {
+    return this.queue.length;
+  };
+}
